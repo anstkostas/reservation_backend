26f052ea9ae318177cbda85c7b26da3cb97abeb3
anstkostas <kanastasiou.dev@gmail.com>
2025-12-15 21:00:22 +0200

    Feat: Created user model & its associations.

---
+++ src/models/user.js
@@ -0,0 +1,57 @@
+module.exports = (sequelize, dataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: dataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: dataTypes.UUIDV4,
+      },
+      firstname: {
+        type: dataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: "",
+      },
+      lastname: {
+        type: dataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: "",
+      },
+      email: {
+        type: dataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: dataTypes.STRING(100),
+        allowNull: false,
+      },
+      role: {
+        type: dataTypes.ENUM("customer", "owner"),
+        allowNull: false,
+        defaultValue: "customer",
+      },
+    },
+    {
+      tableName: "Users",
+      timestamps: false,
+    }
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.Restaurant, {
+      foreignKey: "ownerId",
+      as: "restaurants",
+    });
+    User.hasMany(models.Reservation, {
+      foreignKey: "customerId",
+      as: "reservations",
+    });
+  };
+
+  return User;
+};
