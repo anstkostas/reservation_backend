f8bd10fe89208568893a2d7b96c7379605f57ea7
anstkostas <kanastasiou.dev@gmail.com>
2025-12-15 20:59:40 +0200

    Feat: Created reservation model & its associations.

---
+++ src/models/reservation.js
@@ -0,0 +1,56 @@
+const { DateTime } = require("mssql");
+
+module.exports = (sequelize, dataTypes) => {
+  const Reservation = sequelize.define(
+    "Reservation",
+    {
+      id: {
+        type: dataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: dataTypes.UUIDV4,
+      },
+      date: {
+        type: dataTypes.DATEONLY,
+        allowNull: false,
+      },
+      time: {
+        type: dataTypes.TIME,
+        allowNull: false,
+      },
+      persons: {
+        type: dataTypes.INTEGER,
+        allowNull: false,
+      },
+      status: {
+        type: dataTypes.STRING,
+        allowNull: false,
+      },
+      restaurantId: {
+        type: dataTypes.UUIDV4,
+        allowNull: false,
+      },
+      customerId: {
+        type: dataTypes.UUIDV4,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "Reservations",
+      timestamps: false,
+    }
+  );
+
+  Reservation.associate = (models) => {
+    Reservation.belongsTo(models.Restaurant, {
+      foreignKey: "restaurantId",
+      as: "restaurant",
+    });
+    Reservation.belongsTo(models.User, {
+      foreignKey: "customerId",
+      as: "customer",
+    });
+  };
+
+  return Reservation;
+};
