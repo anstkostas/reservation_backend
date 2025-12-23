5f476cebac2e7fe9b3e31ed6a06a4408d361e59d
anstkostas <kanastasiou.dev@gmail.com>
2025-12-23 22:54:05 +0200

    Feat: Add handler for controllers' response.

---
+++ src/utils/sendResponse.js
@@ -0,0 +1,13 @@
+const defaultMessages = {
+  200: "Request successful",
+  201: "Resource created successfully",
+  204: "No content",
+};
+
+module.exports = {
+  function(res, data, status = 200, message = null) {
+    const payload = { success: true, data };
+    payload.message = message || defaultMessages[status] || "Success";
+    res.status(status).json(payload);
+  },
+};
