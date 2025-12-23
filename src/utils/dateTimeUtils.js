85d336b046c09aedfad8c5de63587cb83680c2cc
anstkostas <kanastasiou.dev@gmail.com>
2025-12-23 22:53:24 +0200

    Fix: Modified implementation of this file to align with app's style.

--- src/utils/dateTimeUtils.js
+++ src/utils/dateTimeUtils.js
@@ -1,39 +1,39 @@
-function isInPast(date) {
-  const d = date instanceof Date ? date : new Date(date);
-  if (isNaN(d)) {
-    throw new Error("Invalid date");
-  }
-  return d < new Date();
-}
+module.exports = {
+  isInPast(date) {
+    const d = date instanceof Date ? date : new Date(date);
+    if (isNaN(d)) {
+      throw new Error("Invalid date");
+    }
+    return d < new Date();
+  },
 
-function getRandomDate() {
-  const today = new Date();
-  const twoMonths = 60 * 24 * 60 * 60 * 1000;
-  const randomOffset =
-    Math.floor(Math.random() * (2 * twoMonths + 1)) - twoMonths;
-  const randomDate = new Date(today.getTime() + randomOffset);
-  const year = randomDate.getFullYear();
-  const month = String(randomDate.getMonth() + 1).padStart(2, "0");
-  const day = String(randomDate.getDate()).padStart(2, "0");
-  const newDate = `${year}-${month}-${day}`;
-  const inPast = isInPast(randomDate);
-  return { newDate, inPast };
-}
+  getRandomDate() {
+    const today = new Date();
+    const twoMonths = 60 * 24 * 60 * 60 * 1000;
+    const randomOffset =
+      Math.floor(Math.random() * (2 * twoMonths + 1)) - twoMonths;
+    const randomDate = new Date(today.getTime() + randomOffset);
+    const year = randomDate.getFullYear();
+    const month = String(randomDate.getMonth() + 1).padStart(2, "0");
+    const day = String(randomDate.getDate()).padStart(2, "0");
+    const newDate = `${year}-${month}-${day}`;
+    const inPast = isInPast(randomDate);
+    return { newDate, inPast };
+  },
 
-function getRandomTime() {
-  const middle = 19;
-  const range = 4;
-  const randomOffset = Math.floor(Math.random() * (2 * range + 1)) - range;
-  const randomTime = middle + randomOffset;
-  return randomTime.toString().concat(":").padEnd(5, 0);
-}
+  getRandomTime() {
+    const middle = 19;
+    const range = 4;
+    const randomOffset = Math.floor(Math.random() * (2 * range + 1)) - range;
+    const randomTime = middle + randomOffset;
+    return randomTime.toString().concat(":").padEnd(5, 0);
+  },
 
-function isWithinTwoMonths(date) {
-  const MAX_MONTHS_AHEAD = 2;
-  const now = new Date();
-  const max = new Date();
-  max.setMonth(max.getMonth() + MAX_MONTHS_AHEAD);
-  return date >= now && date <= max;
-}
-
-module.exports = { isInPast, getRandomDate, getRandomTime, isWithinTwoMonths };
+  isWithinTwoMonths(date) {
+    const MAX_MONTHS_AHEAD = 2;
+    const now = new Date();
+    const max = new Date();
+    max.setMonth(max.getMonth() + MAX_MONTHS_AHEAD);
+    return date >= now && date <= max;
+  },
+};
