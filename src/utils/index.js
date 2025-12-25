94af78df99e5c5a7504fee1bbb670bbb9f953cba
anstkostas <kanastasiou.dev@gmail.com>
2025-12-25 16:16:40 +0200

    Chore: Add new error handler to index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 const dateTimeUtils = require("./dateTimeUtils.js");
 const sendResponse = require("./sendResponse.js");
+const globalErrorHandler = require("./globalErrorHandler.js");
 
 module.exports = {
   dateTimeUtils,
   sendResponse,
+  globalErrorHandler,
 };
