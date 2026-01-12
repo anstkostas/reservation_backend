078121f6d924d7fd0ace9de14006beb3f241c7a7
anstkostas <kanastasiou.dev@gmail.com>
2026-01-12 23:26:14 +0200

    Feat: Removed unused methods & added new.

--- src/utils/dateTimeUtils.js
+++ src/utils/dateTimeUtils.js
@@ -44,21 +44,6 @@ module.exports = {
     }
   },
 
-  // isInPast(date) {
-  //   const d = date instanceof Date ? date : new Date(date);
-  //   if (isNaN(d)) {
-  //     throw new Error("Invalid date");
-  //   }
-  //   return d < new Date();
-  // },
-  // isWithinTwoMonths(date) {
-  //   const MAX_MONTHS_AHEAD = 2;
-  //   const now = new Date();
-  //   const max = new Date();
-  //   max.setMonth(max.getMonth() + MAX_MONTHS_AHEAD);
-  //   return date >= now && date <= max;
-  // },
-
   getRandomDate() {
     const today = new Date();
     const twoMonths = 60 * 24 * 60 * 60 * 1000;
@@ -79,5 +64,17 @@ module.exports = {
     const randomOffset = Math.floor(Math.random() * (2 * range + 1)) - range;
     const randomTime = middle + randomOffset;
     return randomTime.toString().concat(":").padEnd(5, 0);
+  },
+
+  normalizeDBTime(time) {
+    if (time instanceof Date) {
+      const hours = time.getUTCHours().toString().padStart(2, '0');
+      const minutes = time.getUTCMinutes().toString().padStart(2, '0');
+      return `${hours}:${minutes}`;
+    }
+    if (typeof time === 'string') {
+      return time.substring(0, 5);  // (22:00):00.0000000
+    }
+    return time;
   },
 };
