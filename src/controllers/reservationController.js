43d47932eccad07f5b5e555d43833d88b044a34d
anstkostas <kanastasiou.dev@gmail.com>
2026-01-15 00:05:57 +0200

    Feat: Add somewhat informative response messages.

--- src/controllers/reservationController.js
+++ src/controllers/reservationController.js
@@ -11,7 +11,7 @@ module.exports = {
         req.body,
         req.user
       );
-      return sendResponse(res, reservation, 201);
+      return sendResponse(res, reservation, 201, "Reservation created successfully");
     } catch (err) {
       next(err);
     }
@@ -25,7 +25,7 @@ module.exports = {
         req.body,
         req.user
       );
-      return sendResponse(res, updatedReservation);
+      return sendResponse(res, updatedReservation, 200, "Reservation updated successfully");
     } catch (err) {
       next(err);
     }
@@ -38,29 +38,34 @@ module.exports = {
         id,
         req.user
       );
-      return sendResponse(res, canceledReservation);
+      return sendResponse(res, canceledReservation, 200, "Reservation canceled successfully");
     } catch (err) {
       next(err);
     }
   },
 
-  async completeReservation(req, res, next) {
+  async resolveReservation(req, res, next) {
     try {
       const { id } = req.params;
-      const completedReservation = await reservationService.completeReservation(
+      const { status } = req.body;
+      const resolvedReservation = await reservationService.resolveReservation(
         id,
+        status,
         req.user
       );
-      return sendResponse(res, completedReservation);
+      const message = status === 'completed'
+        ? "Reservation successfully updated as completed"
+        : "Reservation successfully updated as no-show";
+      return sendResponse(res, resolvedReservation, 200, message);
     } catch (err) {
       next(err);
     }
   },
 
-  async listActiveReservationsOfOwner(req, res, next) {
+  async listOwnerReservations(req, res, next) {
     try {
       const reservations =
-        await reservationService.listActiveReservationsOfOwner(req.user);
+        await reservationService.listOwnerReservations(req.user);
       return sendResponse(res, reservations);
     } catch (err) {
       next(err);
