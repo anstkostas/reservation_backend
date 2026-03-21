eb0adf4606f068b5ef0ca49150dfb6760972fe00
anstkostas <kanastasiou.dev@gmail.com>
2026-03-21 21:08:32 +0200

    chore: delete orphaned JS files from Phase 10 and 11

--- src/repositories/index.js
+++
@@ -1,9 +0,0 @@
-const restaurantRepository = require("./restaurantRepository.js");
-const userRepository = require("./userRepository.js");
-const reservationRepository = require("./reservationRepository.js");
-
-module.exports = {
-  restaurantRepository,
-  userRepository,
-  reservationRepository,
-};
